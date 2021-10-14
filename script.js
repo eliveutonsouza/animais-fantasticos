36975ab30fb7de4a1f03d486f0e93c2469a37d45
Eliveuton Souza <eliveuton3m@hotmail.com>
2021-10-14 11:32:22 -0300

    update animations

--- script.js
+++ script.js
@@ -1,21 +1,90 @@
-// Duplique o menu e adicione ele em copy
-const menu = document.querySelector('.menu');
-const copy = document.querySelector('.copy');
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-const cloneMenu = menu.cloneNode(true);
-copy.appendChild(cloneMenu);
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
 
-// Selecione o primeiro DT da dl de Faq
-const faq = document.querySelector('.faq');
-const primeiroDt = faq.querySelector('dt');
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
 
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
 
-// Selecione o DD referente ao primeiro DT
-const proximoDD = primeiroDt.nextElementSibling;
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
 
-console.log(proximoDD);
+  if (accordionList.length) {
+    accordionList[0].nextElementSibling.classList.add(activeClass);
 
-// Substitua o conteúdo html de .faq pelo de .animais
-const animais = document.querySelector('.animais');
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
 
-faq.innerHTML = animais.innerHTML;
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+
+    // Forma Alternativa
+    // const topo = section.offsetTop
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // })
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+
+        if (isSectionVisible) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initTabNav();
+initAccordion();
+initScrollSuave();
+initAnimacaoScroll();
