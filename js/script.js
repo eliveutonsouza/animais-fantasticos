e89539c15ddbf06b46e55a65722d217ec95483fc
Eliveuton Souza <eliveuton3m@hotmail.com>
2022-11-29 21:07:47 -0300

    Mudanças aceitas

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import initScrollSuave from "./modules/scroll-suave.js";
-=======
 import ScrollSuave from "./modules/scroll-suave.js";
->>>>>>> f54fa1efd6fcab54f1cd71d4c1963e7b932c277c
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
@@ -13,12 +9,9 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
-<<<<<<< HEAD
-=======
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
->>>>>>> f54fa1efd6fcab54f1cd71d4c1963e7b932c277c
 
 initAnimacaoScroll();
 initAccordion();
